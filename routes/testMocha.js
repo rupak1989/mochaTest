2dc0b488ca58cff86b4d660f75ecdc0635d97564
Node <amrendra@creatiosoft.com>
2017-05-08 18:47:07 +0530

    mocha test module

---
+++ routes/testMocha.js
@@ -0,0 +1,8 @@
+var testMocha = {};
+
+
+testMocha.getTestMochaResult = function(cb){
+		cb({success : true, info : "mocha testing"})
+}
+
+module.exports = testMocha;
---
+++ test/testMocha.js
@@ -0,0 +1,33 @@
+var assert 	= require('assert'),
+	chai 	= require('chai'),
+	fs 		= require('fs'),
+	server 	= require('../app.js'),
+	chaiHttp = require('chai-http');
+
+
+chai.use(chaiHttp);
+
+//mocha testing
+
+
+describe('/GET testMocha', function() {
+		it('test mocha successfully', function(done){ 
+			chai.request(server).get('/testMocha').end(function(err, res){
+				assert.equal(res.body.success, true);
+				done();
+			});
+		});
+
+		setTimeout(function(){
+			it('match output result', function(done){
+				chai.request(server).get('/testMocha').end(function(err, res){
+					console.log('lines 48');
+					assert.equal(res.body.info, "mocha testing");
+					done();
+				});
+			});
+		});
+
+
+
+});
